9baec82b7175db8ca8381f1374aedfd874b658ff
VanehSann <vanessacbsantos22@gmail.com>
2022-03-23 08:07:01 -0300

    erro no github - tentativa de solucionar

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -4,6 +4,7 @@ import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
+// erro no github
 describe('Teste o componente <PokemonDetails.js />', () => {
   test('Se as informações detalhadas do Pokémon selecionado', () => {
     renderWithRouter(<App />);
